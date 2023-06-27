3b08db2e6bfe41e3a504713d14a7b9fd0051f2a5
Brian Barasa <briannbig@gmail.com>
2023-06-27 13:05:24 +0300

    uncompulsory semicolons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log("hello World!");
+console.log("hello World!")
 
-var1 = 10;
-var2 = 20;
-var3 = var1 + var2;
+var1 = 10
+var2 = 20
+var3 = var1 + var2
 
-console.log(var1 + ' + ' + var2 + ' = ' + var3);
+console.log(var1 + ' + ' + var2 + ' = ' + var3)
