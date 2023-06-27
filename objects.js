30c397236a49542f244abff7547d69cc0f6b2c3b
Brian Barasa <briannbig@gmail.com>
2023-06-27 16:39:27 +0300

    the object constructor

--- objects.js
+++ objects.js
@@ -6,3 +6,19 @@ var day = new Date()
 
 console.log('authors: ' + authors);
 console.log('today: ' + day)
+
+//constructing an object
+var book1 = new Object() //creating the object
+book1.subject = "Perl"
+book1.author = "doe"
+console.log("book one:");
+console.log(book1);
+
+function book(subject, name) {
+    this.subject = subject;
+    this.name = name;
+}
+
+var book2 = new book("PHP", "sam")
+console.log("book two: ");
+console.log(book2);
