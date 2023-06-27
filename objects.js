3cda248b9f95f5a1f62e425119e0072d5885b83d
Brian Barasa <briannbig@gmail.com>
2023-06-27 16:30:58 +0300

    javascript objects: new keyword

---
+++ objects.js
@@ -0,0 +1,8 @@
+// the new operator
+
+var employee = new Object();
+var authors = new Array("Sam", "doe", 2)
+var day = new Date()
+
+console.log('authors: ' + authors);
+console.log('today: ' + day)
