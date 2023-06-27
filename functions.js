af8393f0d98a13b254c60dfb24cb01be3187a40b
Brian Barasa <briannbig@gmail.com>
2023-06-27 15:43:42 +0300

    nested functions

--- functions.js
+++ functions.js
@@ -5,17 +5,16 @@ function sayHello() {
     console.log("Hello functions!");
 }
 sayHello();
-
+// function with params
 function printUserDetails(username, email, age, city, country) {
     var isAdult = age >= 18
     console.log(username + ' of email: ' + email + ' from ' + city + ', ' + country + ' is ' + age + ' years old so is he an adult? ...');
     console.log(isAdult ? 'yes' : 'no');
 }
-
+// function with return statement
 function add(var1, var2) {
     return var1 + var2;
 }
-
 
 username = "Sam doe"
 email = "sam@doe.com"
@@ -26,3 +25,11 @@ country = "Kenya"
 printUserDetails(username, email, age, city, country);
 var1 = 10; var2 = 23;
 console.log(var1 + ' + ' + var2 + ' = ' + add(10, 39));
+
+// nested functions **function literals
+console.log('------Nested Functions------');
+function hypotenuse(a, b) {
+    function square(x) { return x * x; }
+    return Math.sqrt(square(a) + square(b));
+}
+console.log(hypotenuse(3, 5));
