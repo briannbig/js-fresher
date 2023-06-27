b04234786b523b88fb42f52e4537a1fc4c827dcd
Brian Barasa <briannbig@gmail.com>
2023-06-27 14:34:05 +0300

    js variables

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ var2 = 20
 var3 = var1 + var2
 
 console.log(var1 + ' + ' + var2 + ' = ' + var3)
+
+
+//declaring variables
+var username, email, age;
+
+var city;
+var country;
+
+username = "Sam doe"
+email = "sam@doe.com"
+age = 25;
+city = "Nairobi"
+country = "Kenya"
+
+var isAdult = age >= 18
+
+console.log(username + ' of email: ' + email+ ' from ' + city + ', ' + country +' is ' + age + ' years old so is he an adult? ...' );
+console.log(isAdult ? 'yes': 'no');
