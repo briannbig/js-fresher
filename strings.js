6de220da6fea7f9cea363884a54e3853e3062236
Brian Barasa <briannbig@gmail.com>
2023-06-28 14:20:11 +0300

    prorotype property

--- strings.js
+++ strings.js
@@ -9,3 +9,17 @@ console.log('string.constructor: ' + string.constructor); // prints string.const
 
 // string length
 console.log('string length: ' + string.length); // .length prints 14
+
+// prototype property: allows you to add properties and methods to any object
+function book(title, author) {
+    this.title = title
+    this.author = author
+}
+
+var myBook = new book("php", "doe")
+book.prototype.price = null
+myBook.price = 23.54
+
+console.log('Book title: ' + myBook.title);
+console.log('Book author: ' + myBook.author);
+console.log('Book price: ' + myBook.price);
